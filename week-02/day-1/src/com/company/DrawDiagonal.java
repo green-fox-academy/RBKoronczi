0195e6f9093d648fbe15e75518264904f6a940fc
Richard Koronczi <rbkoronczi@gmail.com>
2018-10-29 17:31:37 +0100

    Added DrawDiagonal

---
+++ week-02/day-1/src/com/company/DrawDiagonal.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawDiagonal {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // square like this:
+        //
+        //
+        // %%%%%
+        // %%  %
+        // % % %
+        // %  %%
+        // %%%%%
+        //
+        // The square should have as many lines as the number was
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Number: ");
+        int numSide = input.nextInt();
+        for (int i = 0; i < numSide; i++) {
+            if (i == 0 || i == numSide-1) {     //Felső + Alsó él
+                int blocks = numSide;
+                while (blocks > 0) {
+                    System.out.printf("%%");
+                    blocks--;
+                }
+                System.out.println();
+            } else {                            //Oldalak és átlók
+                int blocks = numSide;
+                while (blocks>0) {
+                    if (blocks == numSide || blocks == 1 || blocks == (numSide-i)) {
+                        System.out.printf("%%");
+                    } else {
+                        System.out.printf(" ");
+                    }
+                    blocks--;
+                }
+                System.out.println();
+            }
+        }
+    }
+}
