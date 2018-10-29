5fc10bbc20291bd430d4860d2e5a46e14906e27b
Richard Koronczi <rbkoronczi@gmail.com>
2018-10-29 12:51:00 +0100

    Added DefineBasicInfo

---
+++ week-02/day-1/src/com/company/DefineBasicInfo.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        String name = "Richard Koronczi";
+        // Your age as an integer
+        int age = 25;
+        // Your height in meters as a double
+        double height = 1.78;
+        // Whether you are married or not as a boolean
+        boolean married = false;
+        System.out.println(name + "\n" + age + "\n" + height + "\n" + married );
+    }
+}
