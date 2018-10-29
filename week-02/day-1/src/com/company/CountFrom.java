35ecafb57344591a49fa45e494093bf508116a1c
Richard Koronczi <rbkoronczi@gmail.com>
2018-10-29 15:49:09 +0100

    Added CountFrom

---
+++ week-02/day-1/src/com/company/CountFrom.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CountFrom {
+    public static void main(String[] args) {
+        // Create a program that asks for two numbers
+        // If the second number is not bigger than the first one it should print:
+        // "The second number should be bigger"
+        //
+        // If it is bigger it should count from the first number to the second by one
+        //
+        // example:
+        //
+        // first number: 3, second number: 6, should print:
+        //
+        // 3
+        // 4
+        // 5
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Number One plz: ");
+        int numOne = input.nextInt();
+        System.out.printf("Number Two plz: ");
+        int numTwo = input.nextInt();
+        if (numOne >= numTwo) {
+            System.out.println("The second number should be bigger");
+        } else {
+            while (numOne < numTwo) {
+                System.out.println(numOne++);
+            }
+        }
+    }
+}
