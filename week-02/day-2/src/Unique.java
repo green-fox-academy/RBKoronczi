a2ffae2dd095a0803a091a148296d1b96dbae42c
Richard Koronczi <rbkoronczi@gmail.com>
2018-10-30 16:21:03 +0100

    Added Unique

---
+++ week-02/day-2/src/Unique.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+public class Unique {
+    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as a parameter
+        //  Returns a list of numbers where every number in the list occurs only once
+
+        //  Example
+        System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+        //  should print: `[1, 11, 34, 52, 61]`
+        
+    }
+    public static String unique(int... numbers) {
+        String uniquePrint ="";
+        int uniqueCount =0;
+        // Counting Unique Elements
+        for (int i = 0; i < numbers.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < numbers.length; j++) {
+
+                if (numbers[i] == numbers[j] && i > j) {
+                    isUnique = false;
+                }
+            }
+            if (isUnique) {
+                uniqueCount++;
+            }
+        }
+        //Creating Array of Unique Elements
+        int[] uniqueArray = new int[uniqueCount];
+        int shift = 0;
+        for (int i = 0; i < uniqueArray.length+shift; i++) {
+            boolean isUnique = true;
+
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[i] == numbers[j] && i > j) {
+                    isUnique = false;
+                }
+
+            }
+            if (isUnique) {
+                uniqueArray[i-shift] = numbers[i];
+            } else
+            {
+                shift++;
+            }
+        }
+
+        uniquePrint = Arrays.toString(uniqueArray);
+        return uniquePrint;
+    }
+}
