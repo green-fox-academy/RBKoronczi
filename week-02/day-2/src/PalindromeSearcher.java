c8e85ab11cfbbc843538b896b4b1dafc4f2abdaa
Richard Koronczi <rbkoronczi@gmail.com>
2018-10-31 12:12:47 +0100

    Added PalindromeSearcher (finished)

--- week-02/day-2/src/PalindromeSearcher.java
+++ week-02/day-2/src/PalindromeSearcher.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PalindromeSearcher {
@@ -6,28 +7,46 @@ public class PalindromeSearcher {
     // It should take a string, search for palindromes that at least 3 characters long
     // and return a list with the found palindromes.
     public static void main(String[] args) {
-        System.out.println(PalindromeSearcher("dog goat dad duck doodle never"));
+        System.out.println(Arrays.toString(PalindromeSearcher("dog goat dad duck doodle never")));
+
     }
     public static Object[] PalindromeSearcher (String input) {
         char[] word = Anagram.StringToCharr(input);
-        List<String> anagramList = new ArrayList<>();
-        for (int i = 0; i < word.length; i++) { //Starting from Char[i]
-            for (int j = i+2; j < word.length; j++) {
-                if (word[i] == word[j]){
+        List<String> palindromeList = new ArrayList<>();
+        for (int i = 0; i < word.length; i++) { //Starting from Char[i] Odd palindromes
+            for (int j = i-1, k = i+1; j >=0 && k < word.length; j--, k++) {
+
+                if (word[j] == word[k] ) {
+
                     String adder = "";
-                    for (int k=0;(k+i) < j; k++) {
-                        if (word[k + i] != word[j - k]) {
-                            adder += word[k + i];
-                        } else {
-
-                        }
+                    for (int cc = j; cc <= k; cc++) {
+                        adder += Character.toString(word[cc]);
                     }
-
-                    anagramList.add(adder);
+                    palindromeList.add(adder);
+                }  else {
+                    break;
                 }
 
             }
+
         }
-        return anagramList.toArray();
+        for (int i = 0; i < word.length; i++) { //Starting from Char[i] Even palindromes
+            for (int j = i-1, k = i+2; j >=0 && k < word.length; j--, k++) {
+
+                if (word[j] == word[k] && word[i] == word[i+1] ) {
+
+                    String adder = "";
+                    for (int cc = j; cc <= k; cc++) {
+                        adder += Character.toString(word[cc]);
+                    }
+                    palindromeList.add(adder);
+                }  else {
+                    break;
+                }
+
+            }
+
+        }
+        return (palindromeList.toArray());
     }
 }
