a705da192ec4062bc88d41853c94904dcab0662c
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-12 12:23:48 +0100

    Added CopyFile

---
+++ week-04/day-1/src/CopyFile.java
@@ -0,0 +1,23 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CopyFile {
+    // Write a function that copies a file to an other
+    // It should take the filenames as parameters
+    // It should return a boolean that shows if the copy was successful
+    public static void main(String[] args) {
+        
+    }
+    public static boolean CopyFile (String source, String output) {
+        Path sourcePath = Paths.get(source);
+        Path outputPath = Paths.get(output);
+        try {
+            Files.copy(sourcePath, outputPath );
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
+
