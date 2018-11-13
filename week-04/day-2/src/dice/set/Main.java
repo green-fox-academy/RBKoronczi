3abb9c7cefccebe49f32a161347ed7c1b6465835
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-13 14:31:56 +0100

    Added Dice

---
+++ week-04/day-2/src/dice/set/Main.java
@@ -0,0 +1,19 @@
+package dice.set;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        DiceSet dice = new DiceSet();
+        int[] sixArray = {6, 6, 6, 6, 6, 6};
+        dice.reroll();
+        while (!Arrays.equals(dice.getCurrent(), sixArray)){
+            for (int i = 0; i < 6; i++){
+                if(dice.getCurrent(i) != 6){
+                    dice.reroll(i);
+                }
+            }
+            System.out.println(Arrays.toString(dice.getCurrent()));
+        }
+    }
+}
