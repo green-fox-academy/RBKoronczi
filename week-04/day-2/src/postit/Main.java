614980a2cd2a36d4ffe801346c103a0b9dbecc7e
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-13 11:43:37 +0100

    Added Postit

---
+++ week-04/day-2/src/postit/Main.java
@@ -0,0 +1,11 @@
+package postit;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        PostIt first = new PostIt(Color.ORANGE, "Idea 1", Color.BLUE);
+        PostIt second = new PostIt(Color.PINK, "Awesome", Color.BLACK);
+        PostIt third = new PostIt(Color.YELLOW, "Superb!", Color.GREEN);
+    }
+}
