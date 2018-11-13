f0a82946d3466a29e14e481a698e236265457ae1
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-13 16:07:22 +0100

    Refactor DiceSet

--- week-04/day-2/src/dominoes/Dominoes.java
+++ week-04/day-2/src/dominoes/Dominoes.java
@@ -9,30 +9,29 @@ public class Dominoes {
         // You have the list of Dominoes
         // Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
         // eg: [2, 4], [4, 3], [3, 5] ...
-        System.out.println(dominoes);
         List<Domino> sortedDominoes = new ArrayList<>();
         boolean breakCondition = true;
         int runCounter = 0;
-        while (breakCondition){
-            for (Domino domino : dominoes){
+        while (breakCondition) {
+            for (Domino domino : dominoes) {
                 int addIndex = -1;
-                if (sortedDominoes.size() == 0){
+                if (sortedDominoes.size() == 0) {
                     sortedDominoes.add(domino);
                 } else if (!sortedDominoes.contains(domino)) {
-                    for(Domino sortedDomino : sortedDominoes){
-                        if (domino.getLeftSide() == sortedDomino.getRightSide()){
+                    for (Domino sortedDomino : sortedDominoes) {
+                        if (domino.getLeftSide() == sortedDomino.getRightSide()) {
                             addIndex = sortedDominoes.indexOf(sortedDomino) + 1;
-                        } else if (domino.getRightSide() == sortedDomino.getLeftSide()){
+                        } else if (domino.getRightSide() == sortedDomino.getLeftSide()) {
                             addIndex = sortedDominoes.indexOf(sortedDomino);
                         }
                     }
-                    if (addIndex != -1){
+                    if (addIndex != -1) {
                         sortedDominoes.add(addIndex, domino);
                     }
                 }
             }
             runCounter++;
-            breakCondition = sortedDominoes.size() != dominoes.size() &&  runCounter < dominoes.size()*2;
+            breakCondition = sortedDominoes.size() != dominoes.size() && runCounter < dominoes.size() * 2;
         }
         dominoes.clear();
         dominoes.addAll(sortedDominoes);
@@ -47,7 +46,6 @@ public class Dominoes {
         dominoes.add(new Domino(6, 7));
         dominoes.add(new Domino(2, 4));
         dominoes.add(new Domino(7, 1));
-        dominoes.add(new Domino(8, 8));
         return dominoes;
     }
 }
