3d4ae87b6e6a3bdd9efff75941cdeb0c8140e00f
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-13 09:38:54 +0100

    Added Homework (day2)[5~[5~[5~[D[D[D[D[D[D[Cw[3~[3~[C[C[C[D[B[D[A[C[2~w

---
+++ week-04/day-2/src/homework.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class homework {
+    public static void main(String[] args) {
+
+        ArrayList<HashMap> treeList = new ArrayList<>();
+        HashMap <String, Object> tree1 = new HashMap<>();
+        tree1.put("Age", 25);
+        tree1.put("Type", "Birch");
+        tree1.put("Leaf Color", "Green");
+        tree1.put("Sex", "Female");
+        treeList.add(tree1);
+
+        HashMap <String, Object> tree2 = new HashMap<>();
+        tree2.put("Age", 25);
+        tree2.put("Type", "Birch");
+        tree2.put("Leaf Color", "Green");
+        tree2.put("Sex", "Female");
+        treeList.add(tree2);
+
+        HashMap <String, Object> tree3 = new HashMap<>();
+        tree3.put("Age", 25);
+        tree3.put("Type", "Birch");
+        tree3.put("Leaf Color", "Green");
+        tree3.put("Sex", "Female");
+        treeList.add(tree3);
+
+        HashMap <String, Object> tree4 = new HashMap<>();
+        tree4.put("Age", 25);
+        tree4.put("Type", "Birch");
+        tree4.put("Leaf Color", "Green");
+        tree4.put("Sex", "Female");
+        treeList.add(tree4);
+
+        HashMap <String, Object> tree5 = new HashMap<>();
+        tree5.put("Age", 25);
+        tree5.put("Type", "Birch");
+        tree5.put("Leaf Color", "Green");
+        tree5.put("Sex", "Female");
+        treeList.add(tree5);
+    }
+
+}
