e0c8f705831e5b11faf7673eaa6e5382bd5dc7ef
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-15 11:54:11 +0100

    Added String3

---
+++ week-04/day-4/src/String3.java
@@ -0,0 +1,25 @@
+public class String3 {
+  // Given a string, compute recursively a new string where all the
+  // adjacent chars are now separated by a "*".
+  public static void main(String[] args) {
+    String string = String1.getStringInput();
+    System.out.println(putStarsInString(string));
+  }
+
+  public static String putStarsInString(String string){
+    if(string.lastIndexOf("*") == string.length()-2) {
+      return string;
+    } else {
+      int indexOfLastStar;
+      if(string.contains("*")) {
+        indexOfLastStar = string.lastIndexOf("*");
+      } else {
+        indexOfLastStar = -1;
+      }
+
+      string = string.substring(0, indexOfLastStar+2) + "*" + string.substring(indexOfLastStar+2);
+      System.out.println(string);
+      return putStarsInString(string);
+    }
+  }
+}
