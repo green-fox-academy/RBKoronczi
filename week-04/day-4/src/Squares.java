6bd52bd4099ff14b415467ce0919d6e4a0a1a73b
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-15 13:10:02 +0100

    Refactor Squares

--- week-04/day-4/src/Squares.java
+++ week-04/day-4/src/Squares.java
@@ -1,23 +1,25 @@
 import javax.swing.*;
-
 import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import static javax.swing.JFrame.isDefaultLookAndFeelDecorated;
 
 public class Squares {
   public static void mainDraw(Graphics graphics) {
-    drawSquares(4, 0, 0, WIDTH, HEIGHT, graphics);
+    drawSquares(5, 0, 0, HEIGHT, WIDTH, graphics);
   }
 
   public static void drawSquares(int level, int xPos, int yPos, int width, int height, Graphics graphics){
     if (level == 1){
       graphics.drawRect(xPos, yPos, width, height);
     } else {
+      int sWidth = Math.round(width/3);
+      int sHeight = Math.round(height/3);
       graphics.drawRect(xPos, yPos, width, height);
-      drawSquares(level-1, xPos+width/3, yPos, width/3, height/3, graphics);
-      drawSquares(level-1, xPos, yPos+height/3, width/3, height/3, graphics);
-      drawSquares(level-1, xPos+width/3*2, yPos+height/3, width/3, height/3, graphics);
-      drawSquares(level-1, xPos+width/3, yPos+width/3*2, width/3, height/3, graphics);
+      drawSquares(level-1, xPos+sWidth, yPos, sWidth, sHeight, graphics);
+      drawSquares(level-1, xPos, yPos+sHeight, sWidth, sHeight, graphics);
+      drawSquares(level-1, xPos+sWidth*2, yPos+sHeight, width-sWidth*2, sHeight, graphics);
+      drawSquares(level-1, xPos+sWidth, yPos+sHeight*2, sWidth, height-sHeight*2, graphics);
     }
   }
 
@@ -35,6 +37,7 @@ public class Squares {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
     jFrame.pack();
+    panel.setBackground(Color.YELLOW);
   }
 
   static class ImagePanel extends JPanel {
