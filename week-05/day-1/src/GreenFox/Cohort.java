cdccd55227137aca98ec2ab3628eec4e8941ccb2
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-20 12:37:55 +0100

    Added Cohort

---
+++ week-05/day-1/src/GreenFox/Cohort.java
@@ -0,0 +1,33 @@
+package GreenFox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cohort {
+  String name;
+  List<Student> students;
+  List<Mentor> mentors;
+
+  Cohort(String name) {
+    this.name = name;
+    students = new ArrayList<>();
+    mentors = new ArrayList<>();
+  }
+
+  void addStudent(Student student) {
+    this.students.add(student);
+  }
+
+  void addMentor(Mentor mentor) {
+    this.mentors.add(mentor);
+  }
+
+  void info(){
+    System.out.println(
+        "The " + this.name
+        + " cohort has " + this.students.size()
+        + " students and " + this.mentors.size()
+        + " mentors."
+    );
+  }
+}
