8af7cbeed2e716534940fcac9d338b5c3cc3f5d4
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-20 12:32:23 +0100

    Added Sponsor

---
+++ week-05/day-1/src/GreenFox/Sponsor.java
@@ -0,0 +1,35 @@
+package GreenFox;
+
+public class Sponsor extends Person{
+  String company;
+  int hiredStudents;
+
+  Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+  }
+
+  Sponsor() {
+    super();
+    this.company = "Google";
+  }
+
+  void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.name
+        + ", a " + this + age
+        + " year old " + this.gender
+        + " who represents " + this.company
+        + " and hired " + this.hiredStudents
+        + " students so far."
+    );
+  }
+
+  void hire() {
+    hiredStudents++;
+  }
+
+  void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+}
