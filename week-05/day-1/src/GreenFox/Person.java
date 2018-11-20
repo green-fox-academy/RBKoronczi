7cf2406afba0010de2aa01438312f56460bc4004
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-20 12:08:50 +0100

    Added Person

---
+++ week-05/day-1/src/GreenFox/Person.java
@@ -0,0 +1,30 @@
+package GreenFox;
+
+public class Person {
+  String name;
+  int age;
+  String gender;
+
+  Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  Person() {
+    this.name = "Jane Doe";
+    this.age = 30;
+    this.gender = "female";
+  }
+
+  void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender
+    );
+  }
+
+  void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
+
+}
