754afb4b230590d8cd499689d6931a0465f8874a
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-21 15:59:11 +0100

    Added Reservation

---
+++ week-05/day-3/src/reservation/Reservation.java
@@ -0,0 +1,43 @@
+package reservation;
+
+import java.util.List;
+
+public class Reservation implements Reservationy {
+  private String dowBooking;
+  private String codeBooking;
+  private static List<String> usedCodes;
+
+  public Reservation(String dow){
+    this.dowBooking = dow;
+    this.codeBooking = generateCode();
+    usedCodes.add(this.codeBooking);
+  }
+
+  public String getDowBooking() {
+    return dowBooking;
+  }
+
+  public String getCodeBooking() {
+    return codeBooking;
+  }
+
+  public String toString(){
+    return "Booking# " + codeBooking + " for " + dowBooking;
+  }
+
+  private static String generateCode() {
+    String result = "";
+    do {
+     for(int i = 0; i < 8; i++){
+       result += getRandomUppercaseChar();
+     }
+    } while (!usedCodes.contains(result));
+    return result;
+  }
+
+  private static char getRandomUppercaseChar() {
+    String alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int charCode = (int)(Math.random() * alphabet.length());
+    return alphabet.charAt(charCode);
+  }
+}
