f1153d5816a4fce70163c423af58f3e42fb4f949
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-21 12:00:44 +0100

    Added CloneAble

---
+++ week-05/day-3/src/cloneable/Person.java
@@ -0,0 +1,28 @@
+package cloneable;
+
+public class Person {
+  String name;
+  int age;
+  String gender;
+
+  Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  Person() {
+    this("Jane Doe", 30, "female");
+  }
+
+  void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender
+    );
+  }
+
+  void getGoal() {
+    System.out.println("My goal is: Live for the moment!");
+  }
+
+}
---
+++ week-05/day-3/src/cloneable/Student.java
@@ -0,0 +1,42 @@
+package cloneable;
+
+public class Student extends Person {
+  String previousOrganization;
+  int skippedDays;
+
+  Student(String name, int age, String gender, String previousOrganization) {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  Student() {
+    super();
+    this.previousOrganization = "The School of Life";
+    this.skippedDays = 0;
+  }
+
+  void getGoal() {
+    System.out.println("Be a junior software developer");
+  }
+
+  void introduce() {
+    System.out.println(
+        "Hi, I'm " + this.name
+            + ", a " + this.age
+            + " year old " + this.gender
+            + " from " + this.previousOrganization
+            + " who skipped " + this.skippedDays
+            + " days from the course already."
+    );
+  }
+
+  void skipDays(int numberOfDays) {
+    this.skippedDays += numberOfDays;
+  }
+
+  @Override
+  public Student clone() {
+    return new Student(this.name, this.age, this.gender, this.previousOrganization);
+  }
+}
