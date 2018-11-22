b38b147e96f2b804546a0018e10267fc809bc04f
Richard Koronczi <rbkoronczi@gmail.com>
2018-11-22 11:27:13 +0100

    Added Anagram

--- week-05/day-4/src/AnagramCheck.java
+++ week-05/day-4/src/AnagramCheck.java
@@ -1,7 +1,26 @@
+import java.util.HashMap;
+
 public class AnagramCheck {
   public static boolean anagramCheck(String wordOne, String wordTwo) {
-    
+    wordOne = wordOne.toLowerCase();
+    wordTwo = wordTwo.toLowerCase();
+    HashMap<Character, Integer> wordOneCharacterSet = createHashMapFromString(wordOne);
+    HashMap<Character, Integer> wordTwoCharacterSet = createHashMapFromString(wordTwo);
+    return (wordOneCharacterSet.equals(wordTwoCharacterSet));
+  }
 
-    return false;
+  private static HashMap<Character, Integer> createHashMapFromString(String string){
+    HashMap<Character, Integer> characterMap = new HashMap();
+    char[] characters = string.toCharArray();
+    for (char character : characters) {
+      if(character == ' ') {
+      } else if(characterMap.containsKey(character)) {
+        int qty = characterMap.get(character);
+        characterMap.put(character, qty + 1);
+      } else {
+        characterMap.put(character, 1);
+      }
+    }
+      return characterMap;
   }
 }
--- week-05/day-4/test/AnagramCheckTest.java
+++ week-05/day-4/test/AnagramCheckTest.java
@@ -9,4 +9,24 @@ class AnagramCheckTest {
     assertTrue(AnagramCheck.anagramCheck("Binary", "Brainy"));
   }
 
+  @Test
+  public void anagramTest2() {
+    assertTrue(AnagramCheck.anagramCheck("roast beef", "eat for BSE"));
+  }
+
+  @Test
+  public void anagramTest3() {
+    assertTrue(AnagramCheck.anagramCheck("forty five", "over fifty"));
+  }
+
+  @Test
+  public void anagramTest4() {
+    assertTrue(AnagramCheck.anagramCheck("William Shakespeare", "I am a weakish speller"));
+  }
+
+  @Test
+  public void anagramTest5() {
+    assertTrue(AnagramCheck.anagramCheck("Madam Curie", "Radium came"));
+  }
+
 }
