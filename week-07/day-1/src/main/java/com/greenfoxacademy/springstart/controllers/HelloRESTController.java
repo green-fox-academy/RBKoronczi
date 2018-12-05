32731b91f1bb9d5da15449d1bbec7541374f5f86
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-05 12:53:00 +0100

    Refactor HelloRestController

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,13 +1,14 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
-
-  @RequestMapping
-  public Greeting greet() {
-      return new Greeting(0, "Hello TipTop");
+  
+  @RequestMapping (value="/greeting")
+  public Greeting greet(@RequestParam("name") String name) {
+      return new Greeting(0, "Hello " + name);
   }
 }
