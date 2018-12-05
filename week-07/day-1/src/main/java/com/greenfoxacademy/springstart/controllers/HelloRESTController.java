97ef2b10caa858d7138dfac134cc9718e4d469d0
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-05 12:38:05 +0100

    Feat: HelloRestController

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,7 +1,9 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloRESTController {
 
   @RequestMapping
