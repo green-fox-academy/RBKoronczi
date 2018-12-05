971106da0650410d5f2c3bc8ef240f09d42f5d34
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-05 14:30:10 +0100

    Refactor HelloWebController

--- week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-07/day-1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -3,12 +3,16 @@ package com.greenfoxacademy.springstart.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
+
   @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  public String greeting(Model model, @RequestParam("name") String name) {
+    Greeting greeting = new Greeting(name);
+    model.addAttribute("name", greeting.getContent());
+    model.addAttribute("counter", greeting.getId());
     return "greeting";
   }
 }
