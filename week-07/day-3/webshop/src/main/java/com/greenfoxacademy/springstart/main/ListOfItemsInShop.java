674b8cafb187ca78a56ccd7d25a139bf0f9e3099
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-07 11:23:48 +0100

    feat fileRead

---
+++ week-07/day-3/webshop/src/main/java/com/greenfoxacademy/springstart/main/ListOfItemsInShop.java
@@ -0,0 +1,20 @@
+package com.greenfoxacademy.springstart.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListOfItemsInShop {
+  private List<ShopItem> shopItemList;
+
+  ListOfItemsInShop() {
+    shopItemList = new ArrayList<>();
+  }
+
+  public List<ShopItem> getShopItemList() {
+    return shopItemList;
+  }
+
+  public void getItemsFromFile() {
+
+  }
+}
---
+++ week-07/day-3/webshop/src/main/java/com/greenfoxacademy/springstart/main/ShopItemFileHandler.java
@@ -0,0 +1,43 @@
+package com.greenfoxacademy.springstart.main;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopItemFileHandler {
+  private static String sourceName = "items.txt";
+
+  public static List<ShopItem> readItemsFromFile() {
+    List<String> content = readFile();
+    List<ShopItem> shopItemList = new ArrayList<>();
+    for (String row: content) {
+      shopItemList.add(getItemFromRow(row));
+    }
+    return shopItemList;
+  }
+
+  private static ShopItem getItemFromRow(String row) {
+    String[] itemParts = row.split(";");
+    if(itemParts.length > 3) {
+      String name = itemParts[0];
+      String description = itemParts[1];
+      double price = Double.parseDouble(itemParts[2]);
+      int qty = Integer.parseInt(itemParts[3]);
+      return new ShopItem(name, description, price, qty);
+    }
+    return null;
+  }
+
+  private static List<String> readFile() {
+    Path source = Paths.get(sourceName);
+    try {
+      return Files.readAllLines(source);
+    } catch (IOException eX) {
+      System.err.println(eX.getMessage());
+      return null;
+    }
+  }
+}
