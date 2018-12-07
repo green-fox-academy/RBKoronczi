34b798eb4ac3f51296d2b8a5a623d0fb54deff27
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-07 10:22:29 +0100

    Added ShopItem

---
+++ week-07/day-3/webshop/src/main/java/com/greenfoxacademy/springstart/main/ShopItem.java
@@ -0,0 +1,31 @@
+package com.greenfoxacademy.springstart.main;
+
+public class ShopItem {
+  private String name;
+  private String description;
+  private double price;
+  private int qty;
+
+  public ShopItem(String name, String description, double price, int qty) {
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.qty = qty;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public int getQty() {
+    return qty;
+  }
+}
