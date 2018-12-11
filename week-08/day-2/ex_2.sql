7b79a4b07ae00c0fb032c52bc4a4e5a490f2bf1b
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-11 17:22:37 +0100

    Added Excersize 3/Q4

--- week-08/day-2/ex_2.sql
+++ week-08/day-2/ex_2.sql
@@ -24,3 +24,11 @@ INNER JOIN Highschooler l ON l.ID = Likes.ID2
 INNER JOIN Likes x ON x.ID1 = l.ID
 INNER JOIN Highschooler h2 ON h2.ID = x.ID2
 WHERE h2.name = h.name AND h.name < l.name;
+
+--#Q4
+SELECT DISTINCT h.name, h.grade
+FROM Highschooler h
+LEFT JOIN Likes l1 ON l1.ID1 = h.ID
+LEFT JOIN Likes l2 ON l2.ID2 = h.ID
+WHERE l1.ID1 IS NULL AND l2.ID2 IS NULL
+ORDER BY h.grade, h.name;
