1bfc68d95bc1523b00b7ed33ac2ae5b1f3c8874b
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-13 14:35:27 +0100

    Added CaesarEncoder

--- week-08/day-4/useful-apps/src/main/java/com/greenfox/usefulapps/Controllers/UtilityController.java
+++ week-08/day-4/useful-apps/src/main/java/com/greenfox/usefulapps/Controllers/UtilityController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UtilityController {
-  @Autowired
   UtilityService service;
+
+  @Autowired
+  UtilityController(UtilityService service) {
+        this.service = service;
+  }
 
   @GetMapping("/")
   public String index() {
@@ -32,10 +36,26 @@ public class UtilityController {
   public String validateEmail(@RequestParam("email") String address, Model model) {
     model.addAttribute("text",
         service.emailValidator(address)
-    ? address + " is a valid email address"
-    : address + " is not a valid email address"
+            ? address + " is a valid email address"
+            : address + " is not a valid email address"
     );
-    model.addAttribute("textColor", service.emailValidator(address)? "green" : "red");
+    model.addAttribute("textColor", service.emailValidator(address) ? "green" : "red");
     return "validator";
+  }
+
+  @GetMapping("useful/caesar")
+  public String caesarEncode(
+      @RequestParam("text") String text,
+      @RequestParam("number") int number,
+      @RequestParam("var") String var,
+      Model model) {
+    if(var.equals("encode")) {
+      model.addAttribute("text",
+          service.caesar(text, number));
+    } else {
+      model.addAttribute("text",
+          service.caesar(text, -number));
+    }
+    return "caesar";
   }
 }
--- week-08/day-4/useful-apps/src/main/java/com/greenfox/usefulapps/Services/UtilityService.java
+++ week-08/day-4/useful-apps/src/main/java/com/greenfox/usefulapps/Services/UtilityService.java
@@ -25,10 +25,23 @@ public class UtilityService {
   }
 
   public boolean emailValidator(String address) {
-    if(address.contains("@") && address.indexOf("@") == address.lastIndexOf("@")) {
+    if (address.contains("@") && address.indexOf("@") == address.lastIndexOf("@")) {
       int index = address.indexOf("@");
       return address.substring(index).contains(".");
     }
     return false;
+  }
+
+  public String caesar(String text, int number) {
+    if (number < 0) {
+      number = 26 + number;
+    }
+
+    String result = "";
+    for (int i = 0; i < text.length(); i++) {
+      int offset = Character.isUpperCase(text.charAt(i)) ? 'A' : 'a';
+      result += (char) (((int) text.charAt(i) + number - offset) % 26 + offset);
+    }
+    return result;
   }
 }
