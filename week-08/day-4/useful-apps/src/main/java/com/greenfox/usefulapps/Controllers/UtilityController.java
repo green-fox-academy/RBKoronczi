7ed4f0d402ec9e8d2584367a938996e721432819
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-13 13:09:10 +0100

    feat EmailValidator

--- week-08/day-4/useful-apps/src/main/java/com/greenfox/usefulapps/Controllers/UtilityController.java
+++ week-08/day-4/useful-apps/src/main/java/com/greenfox/usefulapps/Controllers/UtilityController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UtilityController {
@@ -20,5 +21,16 @@ public class UtilityController {
   public String colored(Model model) {
     model.addAttribute("color", service.randomColor());
     return "colored";
+  }
+
+  @GetMapping("/useful/email")
+  public String validateEmail(@RequestParam("email") String address, Model model) {
+    model.addAttribute("text",
+        service.emailValidator(address)
+    ? address + "is a valid email address"
+    : address + "is not a valid email address"
+    );
+    model.addAttribute("textColor", service.emailValidator(address)? "green" : "red");
+    return "validator";
   }
 }
