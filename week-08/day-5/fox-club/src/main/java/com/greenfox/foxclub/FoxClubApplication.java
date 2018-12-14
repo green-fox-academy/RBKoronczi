ec1be8bfc82b3a5278724d44abaf417362d8c1f2
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-14 09:47:10 +0100

    refactor fox-club project

--- week-08/day-5/fox-club/src/main/java/com/greenfox/foxclub/FoxClubApplication.java
+++ week-08/day-5/fox-club/src/main/java/com/greenfox/foxclub/FoxClubApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoxClubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoxClubApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FoxClubApplication.class, args);
+  }
 
 }
 
--- week-08/day-5/fox-club/src/test/java/com/greenfox/foxclub/FoxClubApplicationTests.java
+++ week-08/day-5/fox-club/src/test/java/com/greenfox/foxclub/FoxClubApplicationTests.java
@@ -9,9 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class FoxClubApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+  @Test
+  public void contextLoads() {
+  }
 
 }
 
