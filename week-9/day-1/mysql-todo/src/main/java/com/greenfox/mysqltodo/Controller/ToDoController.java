142baf71f5a3918b1e112b1045832030d20809cf
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-17 15:28:08 +0100

    Created isActive filter

--- week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/Controller/ToDoController.java
+++ week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/Controller/ToDoController.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/todo")
@@ -21,10 +23,13 @@ public class ToDoController {
   }
 
   @GetMapping(value = {"/", "/list"})
-  public String list(Model model) {
+  public String list(Model model, @RequestParam(value = "isActive", required = false) boolean isActive) {
     ArrayList<ToDo> listOfTodos = new ArrayList<ToDo>();
     repository.findAll().forEach(toDo -> listOfTodos.add(toDo));
-    model.addAttribute("todos", listOfTodos);
+    model.addAttribute("todos", isActive
+        ? listOfTodos.stream().filter(toDo -> !toDo.isDone()).collect(Collectors.toList())
+        : listOfTodos
+    );
     return "todolist";
   }
 }
--- week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/MysqlTodoApplication.java
+++ week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/MysqlTodoApplication.java
@@ -1,14 +1,27 @@
 package com.greenfox.mysqltodo;
 
+import com.greenfox.mysqltodo.Model.ToDo;
+import com.greenfox.mysqltodo.Repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysqlTodoApplication {
+public class MysqlTodoApplication implements CommandLineRunner {
+  private ToDoRepository repository;
+
+  @Autowired
+  MysqlTodoApplication(ToDoRepository repository) {
+    this.repository = repository;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(MysqlTodoApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+  }
 }
 
