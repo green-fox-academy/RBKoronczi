b1663aab0ab78e72f156b159c8c7e06fe7de64e0
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-17 11:36:03 +0100

    Created Embedded TODO project

---
+++ week-08/day-5/todo-embedded/src/main/java/com/greenfox/todoembedded/TodoEmbeddedApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.todoembedded;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoEmbeddedApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoEmbeddedApplication.class, args);
+  }
+
+}
+
