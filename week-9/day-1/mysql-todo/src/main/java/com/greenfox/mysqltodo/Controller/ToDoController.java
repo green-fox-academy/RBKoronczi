7a5c8363eb1687359e5df1f913e44c19e3da6099
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-17 15:38:57 +0100

    Created ToDoService

--- week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/Controller/ToDoController.java
+++ week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/Controller/ToDoController.java
@@ -1,7 +1,6 @@
 package com.greenfox.mysqltodo.Controller;
 
-import com.greenfox.mysqltodo.Model.ToDo;
-import com.greenfox.mysqltodo.Repository.ToDoRepository;
+import com.greenfox.mysqltodo.Service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,26 +8,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.stream.Collectors;
-
 @Controller
 @RequestMapping("/todo")
 public class ToDoController {
-  private ToDoRepository repository;
+  private ToDoService service;
 
   @Autowired
-  public ToDoController(ToDoRepository repository) {
-    this.repository = repository;
+  public ToDoController(ToDoService service) {
+    this.service = service;
   }
 
   @GetMapping(value = {"/", "/list"})
   public String list(Model model, @RequestParam(value = "isActive", required = false) boolean isActive) {
-    ArrayList<ToDo> listOfTodos = new ArrayList<ToDo>();
-    repository.findAll().forEach(toDo -> listOfTodos.add(toDo));
     model.addAttribute("todos", isActive
-        ? listOfTodos.stream().filter(toDo -> !toDo.isDone()).collect(Collectors.toList())
-        : listOfTodos
+        ? service.listActive()
+        : service.listAll()
     );
     return "todolist";
   }
---
+++ week-9/day-1/mysql-todo/src/main/java/com/greenfox/mysqltodo/Service/ToDoService.java
@@ -0,0 +1,30 @@
+package com.greenfox.mysqltodo.Service;
+
+import com.greenfox.mysqltodo.Model.ToDo;
+import com.greenfox.mysqltodo.Repository.ToDoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ToDoService {
+  private ToDoRepository repository;
+
+  @Autowired
+  ToDoService(ToDoRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<ToDo> listAll() {
+    ArrayList<ToDo> listOfTodos = new ArrayList<ToDo>();
+    repository.findAll().forEach(toDo -> listOfTodos.add(toDo));
+    return listOfTodos;
+  }
+
+  public List<ToDo> listActive() {
+    return listAll().stream().filter(toDo -> !toDo.isDone()).collect(Collectors.toList());
+  }
+}
