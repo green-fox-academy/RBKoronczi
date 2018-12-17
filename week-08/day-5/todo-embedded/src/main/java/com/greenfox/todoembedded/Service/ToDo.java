1e6b879fb07677f2c6436031e4c8d344f0c8e9d8
Richard Koronczi <rbkoronczi@gmail.com>
2018-12-17 11:47:51 +0100

    Added ToDo and ToDoController classes

---
+++ week-08/day-5/todo-embedded/src/main/java/com/greenfox/todoembedded/Service/ToDo.java
@@ -0,0 +1,31 @@
+package com.greenfox.todoembedded.Service;
+
+public class ToDo {
+  private long id;
+  private String title;
+  private boolean isUrgent;
+  private boolean isDone;
+
+  public ToDo(long id, String title, boolean isUrgent, boolean isDone) {
+    this.id = id;
+    this.title = title;
+    this.isUrgent = isUrgent;
+    this.isDone = isDone;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public boolean isUrgent() {
+    return isUrgent;
+  }
+
+  public boolean isDone() {
+    return isDone;
+  }
+}
