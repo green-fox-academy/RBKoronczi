ecfcffbeb136473164270c8a38c2cbc3a195d0fd
Richard Koronczi <rbkoronczi@gmail.com>
2019-01-07 14:50:08 +0100

    Feat: Greeter

--- week-10/day-1/rest/src/main/java/com/greenfox/rest/ApiController.java
+++ week-10/day-1/rest/src/main/java/com/greenfox/rest/ApiController.java
@@ -10,11 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
   @GetMapping("/doubling")
-  public Object doubling(@RequestParam(value = "input", required=false) Integer input) {
-    if(input != null) {
+  public Object doubling(@RequestParam(value = "input", required = false) Integer input) {
+    if (input != null) {
       return new Doubling(input);
     } else {
       return new ErrorMessage("Please provide an input!");
     }
   }
+
+  @GetMapping("/greeter")
+  public Object greeter(
+      @RequestParam(value = "name", required = false) String name,
+      @RequestParam(value = "title", required = false) String title) {
+    if (name == null && title == null) {
+      return new ErrorMessage("Please provide a name and a title!");
+    } else if (name == null) {
+      return new ErrorMessage("Please provide a name!");
+    } else if (title == null) {
+      return new ErrorMessage("Please provide a title!");
+    } else if (name.length() != 0 && title.length() != 0) {
+      return new Greeter(name, title);
+    } else if (name.length() == 0 && title.length() == 0) {
+      return new ErrorMessage("Please provide a name and a title!");
+    } else if (name.length() == 0) {
+      return new ErrorMessage("Please provide a name!");
+    } else {
+      return new ErrorMessage("Please provide a title!");
+    }
+  }
+
+  
 }
+
