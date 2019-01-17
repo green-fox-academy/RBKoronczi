b1cbdc19420f5df4279a96aeb78fa3f5fbdf2241
RBKoronczi <rbkoronczi@gmail.com>
2019-01-17 16:27:12 +0100

    Feat: Reddit App takes usr/pass from ENV

--- project-week-00/reddit-docker/src/main/java/com/greenfox/reddit/WebSecurityConfig.java
+++ project-week-00/reddit-docker/src/main/java/com/greenfox/reddit/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   public UserDetailsService userDetailsService() {
     UserDetails user =
         User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
+            .username(System.getenv("REDDIT_USERNAME"))
+            .password(System.getenv("REDDIT_PASSWORD"))
             .roles("USER")
             .build();
 
